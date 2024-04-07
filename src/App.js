435a0df1d81270ff1e2d12313eadd850b576af7e
abdelfattah-el2 <abdelnasserwaled400@gmil.com>
2024-04-08 01:00:48 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Form from "./components/Form";
+import Getpage from "./components/Getpage";
+
+function App() {
+  return (
+    <div className="text-center text-[#eeeeee]">
+      <h1 className="  text-center font-bold text-5xl my-3"> Log In </h1>
+      <Form />
+      <Getpage />
+    </div>
+  );
+}
+
+export default App;
